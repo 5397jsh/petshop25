78e569f4795717807b54213c406da422d43a3602
5397jsh <5397jsh@gmail.com>
2025-07-21 15:30:37 +0900

    Initial commit

---
+++ src/main/java/edu/sm/Petshop25Application.java
@@ -0,0 +1,13 @@
+package edu.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petshop25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Petshop25Application.class, args);
+    }
+
+}
