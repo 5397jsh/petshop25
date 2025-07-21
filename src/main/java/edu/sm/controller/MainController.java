2b2625dab4c60efeead71c61a4f25c245b2151e3
5397jsh <5397jsh@gmail.com>
2025-07-21 16:17:43 +0900

    0721

---
+++ src/main/java/edu/sm/controller/MainController.java
@@ -0,0 +1,17 @@
+package edu.sm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // index.jsp 호출
+    }
+}
