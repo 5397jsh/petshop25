1bf1e8360e6be1e82e97ea5048e5f3b2e46196ac
5397jsh <5397jsh@gmail.com>
2025-07-25 15:44:40 +0900

    0725 cart 01

--- src/main/java/edu/sm/controller/CartController.java
+++ src/main/java/edu/sm/controller/CartController.java
@@ -1,18 +1,43 @@
 package edu.sm.controller;
 
+import edu.sm.dto.Cart;
+import edu.sm.dto.Cust;
+import edu.sm.service.CartService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/cart")
+@RequiredArgsConstructor
 public class CartController {
 
-    @GetMapping("/cart")
-    public String showCartProducts(Model model) {
-        // 여기에 신상품 목록 불러오는 로직 작성
-        // 예: List<Product> newProducts = productService.getNewProducts();
-        // model.addAttribute("newProducts", newProducts);
+    final CartService cartService;
+    String dir = "/cart";
 
-        return "cart";  // /WEB-INF/views/new.jsp 로 forward됨
+    @RequestMapping("")
+    public String cart(HttpSession session, Model model) throws Exception {
+        // 로그인한 고객 객체 가져오기
+        Cust cust = (Cust) session.getAttribute("loginCust");
+
+        // 로그인 안 했으면 로그인 페이지로 리다이렉트
+        if (cust == null) {
+            return "redirect:/login";
+        }
+
+        // 고객 ID 추출
+        String id = cust.getCust_id();
+
+        // 장바구니 조회
+        List<Cart> list = cartService.findByCustId(id);
+        model.addAttribute("carts", list);
+        model.addAttribute("center", "cart/cart"); // 예: WEB-INF/views/cart/cart.jsp
+
+        return "index"; // 템플릿 메인
     }
 }
--- src/main/webapp/views/login.jsp
+++ src/main/webapp/views/login.jsp
@@ -61,5 +61,26 @@
 </head>
 <body>
 
+<%@ include file="./header.jsp" %>
+
+<main class="container">
+    <h1>로그인 페이지</h1>
+
+</main>
+
+
+
+
+
+
+
+
+<%-- 상단 메뉴바를 눌렀을때 작동하게 되는 script --%>
+<script src="/js/jquery-1.11.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="/js/plugins.js"></script>
+<script src="/js/script.js"></script>
+<%@ include file="./footer.jsp" %>
 </body>
 </html>
